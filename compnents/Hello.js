d74294d423d8250582117aed9520b59083927a72
Mkhanyisi Gamedze <mkhanyisi.g@gmail.com>
2021-10-01 04:32:38 -0400

    App file

---
+++ compnents/Hello.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>
+          Hi, Welcome to Gym Buddy App by Mkhanyisi!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize:32,
+    padding:25,
+    color:"red",
+    backgroundColor:'black',
+  }
+});
